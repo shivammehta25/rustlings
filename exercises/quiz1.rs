52c56243bf874cc7f4c982eea44a0187c5694f93
Shivam Mehta <shivam.mehta25@gmail.com>
2024-02-19 11:36:30 +0100

    Move semantics is a fun

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n <= 40 { 2*n }
+    else { n }
+}
 
 // Don't modify this function!
 #[test]
